cee51efe7125de5fa713a2e49f1c5c8902f965d2
Farnaz Farzam <fafarz@pc11.pool.tp1.uni-duesseldorf.de>
2015-11-17 15:54:57 +0100

    updated

--- deriv.cxx
+++ deriv.cxx
@@ -11,6 +11,31 @@ void print(double* p, const int N, const double dx,
        cout << i*dx + xmin << "\t" << p[i] << endl;
 }
 
+void fillaray(double* p, const int N, const double dx, const double xmin){
+    for(int i=0; i < N; i++){
+      double x = i*dx + xmin;
+      p[i] = exp(-x*x);
+    }
+}
+void deriv(double* p,const int N,const double dx){
+  double q;
+  double s;
+  p[0]=p[1]/(2*dx);
+  p[N-1]=-p[N-2]/(2*dx);
+  
+  for(int i=1; i<N; i++){
+   
+    
+    q=p[i];
+   
+    p[i]=(p[i+1]-s)/(2*dx);
+    s=q;
+    
+    
+  }
+  
+}
+
 
 int main(){
   const int N = 256;
@@ -18,10 +43,17 @@ int main(){
   const double xmax = 15;
   const double xmin = -15;
   const double dx = (xmax-xmin)/(N-1);
+  
 
   // call to function which fills array p here
+  fillaray(p,N,dx,xmin);
+  deriv(p,N,dx);
   // call to functio which calculates the derivative
   print(p,N,dx,xmin);
+  
+
+  
+ 
 
   return 0;
 }
